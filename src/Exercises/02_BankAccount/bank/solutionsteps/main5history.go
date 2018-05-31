7852a47879c8e6e49429792746409787c92971ed
christophberger <christophberger@users.noreply.github.com>
2018-05-31 22:56:13 +0200

    Add os.Exit()

--- src/Exercises/02_BankAccount/bank/solutionsteps/main5history.go
+++ src/Exercises/02_BankAccount/bank/solutionsteps/main5history.go
@@ -119,6 +119,7 @@ bank update <name> <amount>            Deposit or withdraw money.
 bank transfer <name> <name> <amount>   Transfer money between two accounts.
 bank history <name>                    Show an account's transaction history.
 `)
+	os.Exit(1)
 }
 
 func update(name string, amount int) (int, error) {
