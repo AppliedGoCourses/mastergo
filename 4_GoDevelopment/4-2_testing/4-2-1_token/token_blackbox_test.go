7e961f8971c6e09baba0ee55d29fccd570d72257
christophberger <christophberger@users.noreply.github.com>
2021-05-26 18:11:14 +0200

    feat: add blackbox test

---
+++ 4_GoDevelopment/4-2_testing/4-2-1_token/token_blackbox_test.go
@@ -0,0 +1,22 @@
+package token_test
+
+import (
+	"reflect"
+	"testing"
+
+	token "github.com/AppliedGoCourses/mastergo/4_GoDevelopment/4-2_testing/4-2-1_token"
+)
+
+func TestScan(t *testing.T) {
+	input := "  Always  test     your code  "
+	expected := []string{"Always", "test", "your", "code"}
+
+	// Here we call the function to be tested.
+	actual := token.Scan(input)
+
+	// Now we examine the output.
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("Expected: %#v\nActual:   %#v\n", expected, actual)
+	}
+}
